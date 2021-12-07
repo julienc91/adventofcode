fa1fa1c582a51123247c684124ceb8a0ca5fbc5c
julien <git@julienc.io>
2021-12-07 23:24:16 +0100

    07 The Treachery of Whales

---
+++ 07/07.py
@@ -0,0 +1,16 @@
+from typing import Callable
+
+
+def main_(cost: Callable[[int, int], int]) -> int:
+    data = [int(i) for i in input().split(",")]
+    return min(
+        sum(cost(pos, target) for pos in data) for target in range(min(data), max(data))
+    )
+
+
+def main1() -> int:
+    return main_(lambda pos, target: abs(pos - target))
+
+
+def main2() -> int:
+    return main_(lambda pos, target: abs(pos - target) * (abs(pos - target) + 1) // 2)
