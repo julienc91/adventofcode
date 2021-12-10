e1e01b486ad9e40bd8671a9db3af044a4b2cca71
julien <git@julienc.io>
2021-12-10 22:32:21 +0100

    10 Syntax Scoring

---
+++ 10/10.py
@@ -0,0 +1,57 @@
+from typing import Iterator, Optional
+
+pairs = {")": "(", "]": "[", "}": "{", ">": "<"}
+
+
+def parse_input() -> Iterator[str]:
+    try:
+        while line := input().strip():
+            yield line
+    except EOFError:
+        pass
+
+
+def build_stack(line: str) -> tuple[Optional[str], list[str]]:
+    stack = []
+    for c in line:
+        if c in "([{<":
+            stack.append(c)
+            continue
+
+        try:
+            last = stack.pop()
+        except IndexError:
+            return c, stack
+
+        if pairs[c] != last:
+            return c, stack
+
+    return None, stack
+
+
+def main1() -> int:
+    points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    result = 0
+    for line in parse_input():
+        invalid_char, _ = build_stack(line)
+        if invalid_char is not None:
+            result += points[invalid_char]
+    return result
+
+
+def main2() -> int:
+    points = {"(": 1, "[": 2, "{": 3, "<": 4}
+    scores = []
+    for line in parse_input():
+        score = 0
+        invalid_char, stack = build_stack(line)
+        if invalid_char is not None:
+            continue
+        while stack:
+            c = stack.pop()
+            score = score * 5 + points[c]
+        scores.append(score)
+
+    scores.sort()
+    result = scores[len(scores) // 2]
+    return result
