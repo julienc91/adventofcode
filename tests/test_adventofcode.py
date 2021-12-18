f857b0187d925b57e913ee5afc6f7d271751ce8f
julien <git@julienc.io>
2021-12-18 15:16:45 +0100

    split by year

--- tests/test_adventofcode.py
+++ tests/test_adventofcode.py
@@ -4,44 +4,51 @@ import os
 
 import pytest
 
-EXPECTED_RESULTS = [
-    (1, (1557, 1608)),
-    (2, (1690020, 1408487760)),
-    (3, (3687446, 4406844)),
-    (4, (41503, 3178)),
-    (5, (6283, 18864)),
-    (6, (345793, 1572643095893)),
-    (7, (349812, 99763899)),
-    (8, (349, 1070957)),
-    (9, (516, 1023660)),
-    (10, (311949, 3042730309)),
-    (11, (1721, 298)),
-    (12, (4775, 152480)),
-    (13, (785, -1)),
-    (14, (3143, 4110215602456)),
-    (15, (581, 2916)),
-    (16, (938, 1495959086337)),
-    (17, (7381, 3019)),
-    (18, (3691, 4756)),
+EXPECTED_RESULTS = {
+    2021: [
+        (1, (1557, 1608)),
+        (2, (1690020, 1408487760)),
+        (3, (3687446, 4406844)),
+        (4, (41503, 3178)),
+        (5, (6283, 18864)),
+        (6, (345793, 1572643095893)),
+        (7, (349812, 99763899)),
+        (8, (349, 1070957)),
+        (9, (516, 1023660)),
+        (10, (311949, 3042730309)),
+        (11, (1721, 298)),
+        (12, (4775, 152480)),
+        (13, (785, -1)),
+        (14, (3143, 4110215602456)),
+        (15, (581, 2916)),
+        (16, (938, 1495959086337)),
+        (17, (7381, 3019)),
+        (18, (3691, 4756)),
+    ]
+}
+EXPECTED_RESULTS_WITH_YEAR = [
+    (year, *result) for year in EXPECTED_RESULTS for result in EXPECTED_RESULTS[year]
 ]
 
 
-def run_test(puzzle_name: str, func_name: str) -> int:
-    file_name = f"{puzzle_name}.{puzzle_name}"
+def run_test(year: int, puzzle_name: str, func_name: str) -> int:
+    file_name = f"{year}.{puzzle_name}.{puzzle_name}"
     module = importlib.import_module(file_name)
     return getattr(module, func_name)()
 
 
-@pytest.mark.parametrize("puzzle_id, expected_result", EXPECTED_RESULTS)
-def test_puzzle(monkeypatch, puzzle_id: int, expected_result: tuple[int, int]) -> None:
+@pytest.mark.parametrize("year, puzzle_id, expected_result", EXPECTED_RESULTS_WITH_YEAR)
+def test_puzzle(
+    monkeypatch, year: int, puzzle_id: int, expected_result: tuple[int, int]
+) -> None:
     puzzle_name = f"{puzzle_id:02d}"
-    with open(os.path.join(puzzle_name, "input")) as f:
+    with open(os.path.join(str(year), puzzle_name, "input")) as f:
         data = f.read()
 
     monkeypatch.setattr("sys.stdin", io.StringIO(data))
-    result = run_test(puzzle_name, "main1")
+    result = run_test(year, puzzle_name, "main1")
     assert result == expected_result[0]
 
     monkeypatch.setattr("sys.stdin", io.StringIO(data))
-    result = run_test(puzzle_name, "main2")
+    result = run_test(year, puzzle_name, "main2")
     assert result == expected_result[1]
