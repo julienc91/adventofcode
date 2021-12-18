80adbd67bdef454482fd0b4d18d7c34ea4ff0ed1
julien <git@julienc.io>
2021-12-18 21:25:14 +0100

    07 Some Assembly Required

---
+++ 2015/07/07.py
@@ -0,0 +1,72 @@
+from typing import Callable
+
+
+def parse_instructions() -> list[str]:
+    res = []
+    try:
+        while line := input().strip():
+            res.append(line)
+    except EOFError:
+        pass
+    return res
+
+
+def get_value_from_wire_or_raw_signal(
+    wire_or_signal: str, circuit: dict[str, int]
+) -> int:
+    if wire_or_signal.isdigit():
+        return int(wire_or_signal)
+    return circuit[wire_or_signal]
+
+
+def update_circuit(instruction: str, circuit: dict[str, int]) -> None:
+    if "NOT" in instruction:
+        _, v, _, wire = instruction.split()
+        value = get_value_from_wire_or_raw_signal(v, circuit)
+        circuit[wire] = (2 ** 16) - value - 1
+
+    elif "AND" in instruction or "OR" in instruction or "SHIFT" in instruction:
+        v1, operator, v2, _, wire = instruction.split()
+        value1 = get_value_from_wire_or_raw_signal(v1, circuit)
+        value2 = get_value_from_wire_or_raw_signal(v2, circuit)
+
+        operation: Callable[[int, int], int] = {
+            "AND": lambda a, b: a & b,
+            "OR": lambda a, b: a | b,
+            "LSHIFT": lambda a, b: a << b,
+            "RSHIFT": lambda a, b: a >> b,
+        }[operator]
+        circuit[wire] = operation(value1, value2)
+
+    else:
+        v, _, wire = instruction.split()
+        value = get_value_from_wire_or_raw_signal(v, circuit)
+        circuit[wire] = value
+
+
+def compute_circuit(instructions: list[str]) -> dict[str, int]:
+    circuit: dict[str, int] = {}
+    while instructions:
+        instruction = instructions.pop(0)
+        try:
+            update_circuit(instruction, circuit)
+        except KeyError:
+            instructions.append(instruction)
+    return circuit
+
+
+def main1() -> int:
+    instructions = parse_instructions()
+    circuit = compute_circuit(instructions)
+    return circuit["a"]
+
+
+def main2() -> int:
+    instructions = parse_instructions()
+    circuit = compute_circuit(instructions[:])
+
+    instructions = [f"{circuit['a']} -> b"] + [
+        instruction for instruction in instructions if not instruction.endswith(" -> b")
+    ]
+    circuit = compute_circuit(instructions)
+    return circuit["a"]
