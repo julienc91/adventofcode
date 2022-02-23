872059019f2f3982bb29c366194ec3e7c814cc52
julien <git@julienc.io>
2022-02-23 20:59:01 +0100

    25 Clock Signal

---
+++ 2016/25/25.py
@@ -0,0 +1,68 @@
+from typing import Iterator
+
+
+def parse_instructions() -> list[str]:
+    res: list[str] = []
+    try:
+        while line := input().strip():
+            res.append(line)
+    except EOFError:
+        pass
+    return res
+
+
+def execute(instructions: list[str], a: int) -> Iterator[int]:
+    index = 0
+    registries = {"a": a, "b": 0, "c": 0, "d": 0}
+
+    while index < len(instructions):
+        instruction = instructions[index]
+        command, *args = instruction.split()
+        match command:
+            case "cpy":
+                value = registries[args[0]] if args[0] in registries else int(args[0])
+                registries[args[1]] = value
+                index += 1
+            case "inc":
+                registries[args[0]] += 1
+                index += 1
+            case "dec":
+                registries[args[0]] -= 1
+                index += 1
+            case "jnz":
+                value = registries[args[0]] if args[0] in registries else int(args[0])
+                if value != 0:
+                    index += int(args[1])
+                else:
+                    index += 1
+            case "out":
+                value = registries[args[0]] if args[0] in registries else int(args[0])
+                yield value
+                index += 1
+            case _:
+                raise RuntimeError(f"Unknown command: {command}")
+
+
+def main1() -> int:
+    n = 0
+    min_length = 10
+    instructions = parse_instructions()
+
+    while True:
+        stack = []
+        for i, value in enumerate(execute(instructions, n)):
+            stack.append(value)
+            if value not in (0, 1):
+                break
+
+            if value != i % 2:
+                break
+
+            if i >= min_length:
+                return n
+
+        n += 1
+
+
+def main2() -> int:
+    return -1
