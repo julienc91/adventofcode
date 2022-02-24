a386d3868e6a3e2fbd5ac24a4cea27ff80028ecf
julien <git@julienc.io>
2022-02-24 20:29:00 +0100

    02 Corruption Checksum

---
+++ 2017/02/02.py
@@ -0,0 +1,28 @@
+import itertools
+from typing import Iterator
+
+
+def parse_input() -> Iterator[list[int]]:
+    try:
+        while line := input():
+            yield [int(x) for x in line.split()]
+    except EOFError:
+        pass
+
+
+def main1() -> int:
+    res = 0
+    for line in parse_input():
+        a, b = min(line), max(line)
+        res += b - a
+    return res
+
+
+def main2() -> int:
+    res = 0
+    for line in parse_input():
+        for a, b in itertools.permutations(line, 2):
+            if a % b == 0:
+                res += a // b
+                break
+    return res
