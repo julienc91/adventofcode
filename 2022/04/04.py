87742a85f6f09e9fa879e329ae3eab05d947f301
julien <git@julienc.io>
2022-12-04 09:50:18 +0100

    04 Camp Cleanup

---
+++ 2022/04/04.py
@@ -0,0 +1,25 @@
+from collections.abc import Callable
+
+
+def _main(checker: Callable[[tuple[int, int], tuple[int, int]], bool]) -> int:
+    count = 0
+    try:
+        while line := input().strip():
+            range1, range2 = line.split(",")
+            a1, a2 = map(int, range1.split("-"))
+            b1, b2 = map(int, range2.split("-"))
+            if checker((a1, a2), (b1, b2)):
+                count += 1
+    except EOFError:
+        pass
+    return count
+
+
+def main1() -> int:
+    return _main(
+        lambda a, b: a[0] <= b[0] and a[1] >= b[1] or b[0] <= a[0] and b[1] >= a[1]
+    )
+
+
+def main2() -> int:
+    return _main(lambda a, b: len(range(max(a[0], b[0]), min(a[1], b[1]) + 1)) > 0)
