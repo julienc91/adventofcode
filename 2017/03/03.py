e3505ef92116e1235ce5b33cdbf1df1be09ab8b5
julien <git@julienc.io>
2022-12-07 22:27:44 +0100

    03 Spiral Memory

---
+++ 2017/03/03.py
@@ -0,0 +1,62 @@
+from collections.abc import Iterator
+from enum import Enum
+
+
+class Direction(Enum):
+    RIGHT = 1
+    UP = 2
+    LEFT = 3
+    DOWN = 4
+
+
+def iterate_spiral() -> Iterator[tuple[int, int]]:
+    x, y = 0, 0
+    spiral_number = 1
+    direction = Direction.RIGHT
+
+    while True:
+        yield x, y
+
+        match direction:
+            case Direction.RIGHT:
+                x += 1
+                if x == spiral_number:
+                    direction = Direction.UP
+            case Direction.UP:
+                y += 1
+                if y == spiral_number:
+                    direction = Direction.LEFT
+            case Direction.LEFT:
+                x -= 1
+                if x == -spiral_number:
+                    direction = Direction.DOWN
+            case Direction.DOWN:
+                y -= 1
+                if y == -spiral_number:
+                    direction = Direction.RIGHT
+                    spiral_number += 1
+            case _:
+                raise ValueError()
+
+
+def main1() -> int:
+    target = int(input())
+    for counter, (x, y) in enumerate(iterate_spiral(), start=1):
+        if counter >= target:
+            return abs(x) + abs(y)
+    raise RuntimeError()
+
+
+def main2() -> int:
+    target = int(input())
+    mapping: dict[tuple[int, int], int] = {(0, 0): 1}
+
+    for x, y in iterate_spiral():
+        value = sum(
+            mapping.get((x + a, y + b), 0) for a in (-1, 0, 1) for b in (-1, 0, 1)
+        )
+        if value >= target:
+            return value
+
+        mapping[(x, y)] = value
+    raise RuntimeError()
--- tests/test_adventofcode.py
+++ tests/test_adventofcode.py
@@ -42,6 +42,7 @@ EXPECTED_RESULTS: dict[int, list[tuple[int, tuple[int | str, int | str]]]] = {
     2017: [
         (1, (1175, 1166)),
         (2, (21845, 191)),
+        (3, (438, 266330)),
     ],
     2016: [
         (1, (230, 154)),
