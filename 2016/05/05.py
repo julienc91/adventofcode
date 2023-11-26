c1d8d547074a98eb5f1b34ed2c5ccc36887f6049
julien <git@julienc.io>
2023-11-26 17:27:01 +0100

    pre-compute cache in 2016-05

--- 2016/05/05.py
+++ 2016/05/05.py
@@ -1,28 +1,39 @@
 from hashlib import md5
 
+from utils.cache import pre_compute
+
+
+def build_cache(door_id) -> list[str]:
+    res = []
+    for index in range(30_000_000):
+        hash_ = md5(f"{door_id}{index}".encode(), usedforsecurity=False).hexdigest()
+        if hash_.startswith("00000"):
+            res.append(hash_)
+    return res
+
 
 def main1() -> str:
     door_id = input().strip()
-    index = 0
     res = ""
-    while len(res) < 8:
-        if (hash_ := md5(f"{door_id}{index}".encode()).hexdigest()).startswith("00000"):
+
+    with pre_compute(lambda: build_cache(door_id), 2016, 5) as cache:
+        for hash_ in cache:
             res += hash_[5]
-        index += 1
+            if len(res) >= 8:
+                break
     return res
 
 
 def main2() -> str:
     door_id = input().strip()
-    index = 0
-    found = 0
     res = [""] * 8
-    while found < len(res):
-        if (hash_ := md5(f"{door_id}{index}".encode()).hexdigest()).startswith("00000"):
+
+    with pre_compute(lambda: build_cache(door_id), 2016, 5) as cache:
+        for hash_ in cache:
             if hash_[5].isdigit():
                 n = int(hash_[5])
                 if n < len(res) and not res[n]:
                     res[n] = hash_[6]
-                    found += 1
-        index += 1
+                    if all(res):
+                        break
     return "".join(res)
