9dae2736e93842ab9011f8f30ace5a93eea408eb
julien <git@julienc.io>
2023-12-01 23:29:14 +0100

    extend parsing utility

--- utils/parsing.py
+++ utils/parsing.py
@@ -1,9 +1,13 @@
+from typing import TypeVar
 from collections.abc import Iterator
 
 
-def parse_input() -> Iterator[str]:
+T = TypeVar("T")
+
+
+def parse_input(type_: type[T] = str) -> Iterator[T]:
     try:
         while True:
-            yield input()
+            yield type_(input())
     except EOFError:
         pass
