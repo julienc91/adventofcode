aa7c972ab8453aaac988ec65f775f21505e89165
julien <git@julienc.io>
2023-12-08 23:46:13 +0100

    explicit lcm hypothesis

--- 2023/08/08.py
+++ 2023/08/08.py
@@ -1,6 +1,7 @@
 import itertools
 import math
 import re
+from collections.abc import Iterator
 
 from utils.parsing import parse_input
 
@@ -17,27 +18,44 @@ def parse_data() -> tuple[str, dict[str, tuple[str, str]]]:
 
 
 def count_steps(
-    start: str, end_regex: str, pattern: str, mapping: dict[str, tuple[str, str]]
-):
+    start: str,
+    end_regex: str,
+    pattern: Iterator[str],
+    mapping: dict[str, tuple[str, str]],
+) -> tuple[int, str]:
     position = start
     count = 0
-    pattern_it = itertools.cycle(pattern)
-    while not re.match(end_regex, position):
-        direction = 0 if next(pattern_it) == "L" else 1
+    while count == 0 or not re.match(end_regex, position):
+        direction = 0 if next(pattern) == "L" else 1
         position = mapping[position][direction]
         count += 1
-    return count
+    return count, position
 
 
 def main1() -> int:
     pattern, mapping = parse_data()
-    return count_steps("AAA", "ZZZ", pattern, mapping)
+    count, _ = count_steps("AAA", "ZZZ", itertools.cycle(pattern), mapping)
+    return count
+
+
+def assume_lcm_hypothesis(
+    start: str, pattern: str, mapping: dict[str, tuple[str, str]]
+) -> int:
+    pattern_it = itertools.cycle(pattern)
+    count, finish = count_steps(start, r"[A-Z]{2}Z", pattern_it, mapping)
+    if count % len(pattern) != 0:
+        raise ValueError("LCM hypothesis is not correct")
+
+    new_count, new_finish = count_steps(finish, r"[A-Z]{2}Z", pattern_it, mapping)
+    if (new_count, new_finish) != (count, finish):
+        raise ValueError("LCM hypothesis is not correct")
+    return count
 
 
 def main2() -> int:
     pattern, mapping = parse_data()
     step_counts = [
-        count_steps(start, r"[A-Z]{2}Z", pattern, mapping)
+        assume_lcm_hypothesis(start, pattern, mapping)
         for start in mapping
         if start.endswith("A")
     ]
