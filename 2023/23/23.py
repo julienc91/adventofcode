bee0b67e6ff39743769694bae5d3ecf61ea4f36f
julien <git@julienc.io>
2023-12-23 23:38:56 +0100

    perf optimizations (45s -> 10s)

--- 2023/23/23.py
+++ 2023/23/23.py
@@ -8,17 +8,11 @@ from utils.parsing import parse_input
 
 @dataclass
 class Node:
-    position: tuple[int, int]
+    id: int
     is_start: bool = False
     is_end: bool = False
 
-    neighbours: set[tuple["Node", int]] = field(default_factory=set)
-
-    def __hash__(self) -> int:
-        return hash(self.position)
-
-    def __repr__(self) -> str:
-        return f"Node(position={self.position})"
+    neighbours: list[tuple[int, int]] = field(default_factory=list)
 
 
 def get_neighbours(
@@ -55,9 +49,10 @@ def maze_to_graph(maze: list[str], directed: bool) -> list[Node]:
     start = (next(x for x in range(len(maze[0])) if maze[0][x] == "."), 0)
     end = (next(x for x in range(len(maze[-1])) if maze[-1][x] == "."), len(maze) - 1)
 
+    node_counter = itertools.count()
     nodes_by_position = {
-        start: Node(start, is_start=True),
-        end: Node(end, is_end=True),
+        start: Node(id=next(node_counter), is_start=True),
+        end: Node(id=next(node_counter), is_end=True),
     }
 
     for x, y in itertools.product(range(len(maze[0])), range(len(maze))):
@@ -66,59 +61,58 @@ def maze_to_graph(maze: list[str], directed: bool) -> list[Node]:
             continue
 
         neighbours = get_neighbours(x, y, maze, directed=False)
-        assert 1 <= len(neighbours) <= 4, (neighbours, x, y)
-        if len(neighbours) <= 2:
-            continue
-
-        nodes_by_position[(x, y)] = Node((x, y))
+        if len(neighbours) > 2:
+            nodes_by_position[(x, y)] = Node(id=next(node_counter))
 
     queue: deque[tuple[tuple[int, int], Node, int]] = deque(
-        [(Direction.BOTTOM.move(start[0], start[1]), nodes_by_position[start], 0)]
+        [((start[0], start[1] + 1), nodes_by_position[start], 0)]
     )
-    visited: set[tuple[tuple[int, int], Node]] = {(start, nodes_by_position[start])}
+    visited: set[tuple[tuple[int, int], int]] = {(start, nodes_by_position[start].id)}
     while queue:
         position, previous_node, length = queue.popleft()
-        if (position, previous_node) in visited:
+        if (position, previous_node.id) in visited:
             continue
 
         length += 1
         (x, y) = position
-        visited.add((position, previous_node))
+        visited.add((position, previous_node.id))
         if position in nodes_by_position:
             node = nodes_by_position[position]
-            previous_node.neighbours.add((node, length))
+            previous_node.neighbours.append((node.id, length))
             previous_node = node
             length = 0
 
         for next_position in get_neighbours(x, y, maze, directed=directed):
             queue.appendleft((next_position, previous_node, length))
 
-    return list(nodes_by_position.values())
+    return sorted(nodes_by_position.values(), key=lambda n: n.id)
 
 
 def _main(directed: bool) -> int:
     maze = list(parse_input())
     nodes = maze_to_graph(maze, directed=directed)
     start_node = next(node for node in nodes if node.is_start)
+    visited: list[bool] = [False for _ in nodes]
 
-    def inner(node: Node, visited: set[Node]):
+    def inner(node_id: int):
+        node = nodes[node_id]
         if node.is_end:
             return 0
 
         res = -1
-        visited.add(node)
-        for next_node, length in node.neighbours:
-            if next_node in visited:
+        visited[node.id] = True
+        for next_node_id, length in node.neighbours:
+            if visited[next_node_id]:
                 continue
 
-            subtotal = inner(next_node, visited)
-            if subtotal >= 0:
-                res = max(res, length + subtotal)
+            subtotal = inner(next_node_id)
+            if subtotal >= 0 and subtotal >= res - length:
+                res = length + subtotal
 
-        visited.remove(node)
+        visited[node.id] = False
         return res
 
-    return inner(start_node, set())
+    return inner(start_node.id)
 
 
 def main1() -> int:
