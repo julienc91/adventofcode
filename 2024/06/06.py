37a3c6a7fd443bfe404a4d3343752544bea24b12
julien <git@julienc.io>
2024-12-07 09:08:45 +0100

    06 Guard Gallivant - Improved perfs by exploring possible obstacles at the same time as initial path

--- 2024/06/06.py
+++ 2024/06/06.py
@@ -1,8 +1,56 @@
+from collections.abc import Iterator
+
 from utils.parsing import parse_input
 
-Point = tuple[int, int]
 Coordinate = tuple[int, int, int, int]
 Grid = list[list[str]]
+
+
+class GuardPathGenerator:
+    def __init__(
+        self, grid: Grid, start: Coordinate, visited: set[Coordinate] | None = None
+    ):
+        self.grid = grid
+        self.start = start
+        self.visited = visited or set()
+        self._is_infinite_loop = None
+
+    def __iter__(self) -> Iterator[Coordinate]:
+        yield self.start
+        self.visited.add(self.start)
+
+        x, y, dx, dy = self.start
+        while True:
+            x2, y2 = x + dx, y + dy
+            if (x2, y2, dx, dy) in self.visited:
+                self._is_infinite_loop = True
+                break
+
+            if x2 < 0 or y2 < 0:
+                self._is_infinite_loop = False
+                break
+
+            try:
+                value = self.grid[y2][x2]
+            except IndexError:
+                self._is_infinite_loop = False
+                break
+
+            if value != ".":
+                dx, dy = -dy, dx
+                continue
+
+            x, y = x2, y2
+            yield x, y, dx, dy
+            self.visited.add((x, y, dx, dy))
+
+    @property
+    def is_infinite_loop(self) -> bool:
+        for _ in self:
+            pass
+        if self._is_infinite_loop is None:
+            raise RuntimeError()
+        return self._is_infinite_loop
 
 
 def parse_grid() -> tuple[Grid, Coordinate]:
@@ -16,91 +64,33 @@ def parse_grid() -> tuple[Grid, Coordinate]:
     return grid, guard_position
 
 
-def get_guard_path(grid: Grid, start: Coordinate) -> list[Coordinate]:
-    path = []
-    visited = set()
-    x, y, dx, dy = start
-    while True:
-        path.append((x, y, dx, dy))
-        visited.add((x, y, dx, dy))
-
-        x2, y2 = x + dx, y + dy
-        if (x2, y2, dx, dy) in visited or x2 < 0 or y2 < 0:
-            break
-
-        try:
-            value = grid[y2][x2]
-        except IndexError:
-            break
-
-        if value == ".":
-            x, y = x2, y2
-            continue
-
-        dx, dy = -dy, dx
-    return path
-
-
-def is_infinite_loop(grid: Grid, start: Coordinate, visited: set[Coordinate]) -> bool:
-    """Same logic as above, but without keeping track of the path for performance reasons"""
-    x, y, dx, dy = start
-    while True:
-        visited.add((x, y, dx, dy))
-
-        x2, y2 = x + dx, y + dy
-        if (x2, y2, dx, dy) in visited:
-            return True
-
-        if x2 < 0 or y2 < 0:
-            break
-
-        try:
-            value = grid[y2][x2]
-        except IndexError:
-            break
-
-        if value == ".":
-            x, y = x2, y2
-            continue
-
-        dx, dy = -dy, dx
-
-    return False
-
-
 def main1() -> int:
-    grid, guard = parse_grid()
-    path = get_guard_path(grid, guard)
-    return len({(x, y) for x, y, _, _ in path})
-
-
-def build_visited_cache(
-    path: list[Coordinate],
-) -> dict[Point, tuple[Coordinate, set[Coordinate]]]:
-    res = {}
-    visited = {path[0]}
-    for i in range(1, len(path)):
-        x, y, _, _ = path[i]
-        if (x, y) in res:
-            continue
-
-        res[(x, y)] = (path[i - 1], visited.copy())
-        visited.add(path[i])
-    return res
+    grid, start = parse_grid()
+    return len({(x, y) for x, y, _, _ in GuardPathGenerator(grid, start)})
 
 
 def main2() -> int:
-    count = 0
     grid, start = parse_grid()
+    valid_obstacles = set()
 
-    path = get_guard_path(grid, start)
-    visited_cache = build_visited_cache(path)
+    cur_pos = None
+    visited = {start}
+    visited_cells = {(x, y) for x, y, _, _ in visited}
 
-    possible_obstacles = visited_cache.keys()
-    for x, y in possible_obstacles:
-        grid[y][x] = "#"
-        start, visited = visited_cache[(x, y)]
-        if is_infinite_loop(grid, start, visited=visited):
-            count += 1
-        grid[y][x] = "."
-    return count
+    for next_pos in GuardPathGenerator(grid, start):
+        if cur_pos is not None:
+            x, y, _, _ = next_pos
+            if (x, y) in valid_obstacles or (x, y) in visited_cells:
+                continue
+
+            grid[y][x] = "#"
+            generator = GuardPathGenerator(grid, cur_pos, visited=set(visited))
+            if generator.is_infinite_loop:
+                valid_obstacles.add((x, y))
+            grid[y][x] = "."
+
+            visited.add(next_pos)
+            visited_cells.add((x, y))
+
+        cur_pos = next_pos
+    return len(valid_obstacles)
